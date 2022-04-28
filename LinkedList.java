7b5e5c5111522130aed6a76a7cdffd812447dd12
Dimitri Rodriguez <dimitrirodriguez17@gmail.com>
2022-04-27 20:14:10 -0600

    Start of the homework

---
+++ LinkedList.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class LinkedList{
+	public LinkedList(){
+		private int iData;
+		private double Ddata;
+		private Link next;
+	}
+
+	
+
+	public main method (Strg Args[]){
+	}
+}
