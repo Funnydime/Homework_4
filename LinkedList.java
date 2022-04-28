cefa79b81b5309c2121c3628645b8089cbb6ca52
Dimitri Rodriguez <dimitrirodriguez17@gmail.com>
2022-04-27 20:47:11 -0600

    Getting the methods

--- LinkedList.java
+++ LinkedList.java
@@ -1,12 +1,39 @@
 import java.util.*;
 
-public class LinkedList{
-	public LinkedList(){
-		private int iData;
+public class Link{
+	private int iData;
 		private double Ddata;
 		private Link next;
+}
+
+public class LinkedList{
+	private Link head
+	public LinkedList(){
+		
+	}
+	public insertFirst(){
+
 	}
 
+	public insertAfter(){
+
+	}
+
+	public find(){
+
+	}
+
+	public deleteLast(){
+ 
+	}
+
+	public displayList(){
+
+	}
+
+	public isEmpty(){
+		
+	}
 	
 
 	public main method (Strg Args[]){
