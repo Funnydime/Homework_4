58f6b10fdf0fb5afb00641c8adf0757e36a82a22
Dimitri Rodriguez <dimitrirodriguez17@gmail.com>
2022-05-02 22:06:08 -0600

    Got the base down

--- LinkedList.java
+++ LinkedList.java
@@ -1,42 +1,69 @@
 import java.util.*;
+import java.util.Scanner;
 
 public class Link{
 	private int iData;
-		private double Ddata;
-		private Link next;
+	private double dData;
+	private Link next;
+}
+
+public Link(int id, double dd){
+	iData = id;
+	dData = dd;
 }
 
 public class LinkedList{
-	private Link head
+	private Link head;
 	public LinkedList(){
-		public List <integer> list = new LinkedList<integer>;
+		head = null;
 	}
-	public insertFirst(){
-		if (head!=null){
-			head = head;
+	public void insertFirst(int id, double dd){
+		Link newLink = new Link(id,dd);
+		newLink.next = head;
+		head = newLink;
 		}
-
-		else{}
 	}
 
-	public insertAfter(){
-
+	public  void insertAfter(int id, double dd){
+		Link newLink = new Link(id,dd);
+		newLink.next = head;
 	}
 
-	public find(){
-
+	public find(int item){
+		Link current = head;
+		while (current.iData != item)}{
+			if(current.next == null){
+				return null;
+			}
+			else{
+				current = current.next;	
+			}
+		}
+		return current;
 	}
 
-	public deleteLast(){
-		
+	public void deleteLast(){
+		Link current = head;
+		while (current.next != null){
+			current = current.next;
+		}
 	}
 
-	public displayList(){
-
+	public void displayLink(){
+		System.out.print("{" + iData + "," + dData + "}";)
 	}
 
-	public isEmpty(){
+	public void displayList(){
+		Link current = head;
 
+		while(current != null){
+			current.displayLink();
+			current = current.next;
+		}
+	}
+
+	public boolean isEmpty(){
+		return (head == null);
 	}
 	
 
